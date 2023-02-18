8490f724cef1f270cf7f0c27f417ececd5b55ab3
miurachikaa <chikaa719@gmail.com>
2023-02-18 17:26:12 +0900

    cosme投稿機能の単体テスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :item do
-    
+    brand             { 'test' }
+    category_id  { '2' }
+    name              { 'test' }
+
+    association :user
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe 'cosme投稿' do
+    context 'cosme投稿できる場合' do
+      it '全ての項目が正しく入力されていれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+    context 'cosme投稿できない場合' do
+      it 'ブランドが空では保存できない' do
+        @item.brand = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Brand can't be blank")
+      end
+      it 'カテゴリーが空では保存できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+      it '名前が空では保存できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'userが紐付いていないと保存できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
+    end
+  end
 end
