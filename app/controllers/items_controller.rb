66230b91c121ee077e9a2346bf035cc0103ac653
miurachikaa <chikaa719@gmail.com>
2023-02-20 09:08:26 +0900

    cosme編集ページの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,7 +19,17 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+
   end
+
+  def edit
+
+    @item = Item.find(params[:id])
+
+  end
+
+
+
 
   private
 
