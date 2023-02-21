d88fdb533f06d8c332dc884e174b36a5d3c0bca7
miurachikaa <chikaa719@gmail.com>
2023-02-21 15:32:08 +0900

    ユーザーページの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
+  def after_sign_out_path_for(_resource)
+    user_session_path
+  end
+
 
 
   private
