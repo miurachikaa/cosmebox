1922ce874b03915c1bf45e103417f30d1574b8a0
miurachikaa <chikaa719@gmail.com>
2023-03-20 16:49:01 +0900

    スターレーティング

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,7 @@ require("../sign")
 require("../item")
 
 //= require jquery
-//= require jquery.raty
+//= require jquery_raty
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -18,4 +18,7 @@ require("../item")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+window.$ = window.jQuery = require('jquery'); 
+require('packs/raty')
 
