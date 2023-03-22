7a169c58a9cf8610ea0eab89890c2be47df7b32f
miurachikaa <chikaa719@gmail.com>
2023-03-22 15:02:35 +0900

    js削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,8 +9,6 @@ require("channels")
 require("../sign")
 require("../item")
 
-//= require jquery
-//= require jquery_raty
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -19,6 +17,5 @@ require("../item")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-window.$ = window.jQuery = require('jquery'); 
-require('packs/raty')
+
 
